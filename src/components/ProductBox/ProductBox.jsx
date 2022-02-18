adebbdd63465db60184693209b8051e6f68dc4a6
hossein-nahali <82251151+hossein-nahali@users.noreply.github.com>
2022-02-18 16:26:34 +0330

    add link in title product

--- src/components/ProductBox/ProductBox.jsx
+++ src/components/ProductBox/ProductBox.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PriceBox from "./PriceBox";
 import {Box, Typography} from "@mui/material";
 import './ProductBox.scss'
+import {Link} from "react-router-dom";
 
 export default function ProductBox({Obj_P}) {
     return (
@@ -10,7 +11,9 @@ export default function ProductBox({Obj_P}) {
                 <img src={Obj_P.default_img} alt=""/>
             </Box>
             <Box className={'product-content'}>
-                <Typography variant={'h3'} className={'product-title'}>{Obj_P.title}</Typography>
+                <Typography variant={'h3'} className={'product-title'}>
+                    <Link to={`/SingleProduct/${Obj_P.id}`}>{Obj_P.title}</Link>
+                </Typography>
                 <Box className={'product-price'}>
                     <PriceBox price={Obj_P.price} is_show_default_price={Obj_P.is_show_default_price}/>
                 </Box>
